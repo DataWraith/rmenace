796977e1b7720593dbc64b203ac998adc794ffd1
Johannes Holzfuß <Drangon@gmx.de>
2008-05-12 13:15:03 +0200

    Added RandomFinishPlayer

---
+++ lib/player_random_finish.rb
@@ -0,0 +1,28 @@
+require 'player_random.rb'
+
+module TicTacToe
+
+  class RandomFinishPlayer < RandomPlayer
+
+    def name
+      "Random Finish"
+    end
+
+    def select_move(grid)
+      # Play randomly, but connect three if possible
+
+      for i in get_empty_fields(grid)
+	grid.play(i)
+	gamestate = grid.gamestate
+	grid.undo
+
+	if (gamestate != :ongoing) and (gamestate != :tie)
+	  return i
+	end
+      end
+      return yield(grid)
+    end
+
+  end
+
+end
