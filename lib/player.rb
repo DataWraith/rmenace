a29e58d82613692b68b636bbda6da3197f8a345b
Johannes Holzfuß <Drangon@gmx.de>
2008-05-12 13:51:00 +0200

    Player: Made get_empty_fields, select_move and check_for_valid_grid private

--- lib/player.rb
+++ lib/player.rb
@@ -7,6 +7,13 @@ module TicTacToe
       "Player"
     end
 
+    def make_move(grid)
+      check_for_valid_grid(grid)
+      grid.play(select_move(grid))
+    end
+
+    private
+
     def check_for_valid_grid(grid)
       if not grid.instance_of?(TicTacToe::Grid)
 	raise IllegalArgumentError, "Not a grid"
@@ -21,11 +28,6 @@ module TicTacToe
       empty_fields = (0..8).to_a
       empty_fields.delete_if { |x| grid.fields[x] != :empty }
       return empty_fields
-    end
-
-    def make_move(grid)
-      check_for_valid_grid(grid)
-      grid.play(select_move(grid))
     end
 
     def select_move(grid)
