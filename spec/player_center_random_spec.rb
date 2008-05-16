a221adcb6aef0a24187cacb8668e95df4878d1cf
Johannes Holzfuß <Drangon@gmx.de>
2008-05-16 11:14:27 +0200

    Fill in RandomFinishPlayer and CenterRandomPlayer spec-stubs

--- spec/player_center_random_spec.rb
+++ spec/player_center_random_spec.rb
@@ -4,12 +4,16 @@ require File.dirname(__FILE__) + '/player_spec.rb'
 
 describe "A Player playing center when possible, randomly otherwise," do
 
+  it_should_behave_like "A Player"
+
   before(:each) do
     @player = TicTacToe::CenterRandomPlayer.new
   end
 
-  it_should_behave_like "A Player"
-
-  it "should play into the center if that move is legal"
+  it "should play into the center if that move is legal" do
+    my_grid = TicTacToe::Grid.new
+    @player.make_move(my_grid)
+    my_grid.history.last.should == 4
+  end
 
 end
--- spec/player_random_finish_spec.rb
+++ spec/player_random_finish_spec.rb
@@ -10,6 +10,14 @@ describe "A Player making random moves, but connecting three if possible," do
     @player = TicTacToe::RandomFinishPlayer.new
   end
 
-  it "should connect three if possible"
+  it "should connect three if possible" do
+    grid = TicTacToe::Grid.new
+    [0, 1, 3, 4].each do |i|
+      grid.play(i)
+    end
+
+    @player.make_move(grid)
+    grid.gamestate.should == :x_wins
+  end
 
 end
