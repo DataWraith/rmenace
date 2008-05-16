b3875354665bf6bd25453f79fb9a720738eb27f4
Johannes Holzfuß <Drangon@gmx.de>
2008-05-16 21:38:45 +0200

    Filled in the MinimaxPlayer spec-stubs

--- spec/player_minimax_spec.rb
+++ spec/player_minimax_spec.rb
@@ -8,10 +8,22 @@ describe "A perfect Minimax Player" do
 
   before(:each) do
     @player = TicTacToe::MinimaxPlayer.new
+    @grid = TicTacToe::Grid.new
+    [0, 1, 3, 4].each do |i|
+      @grid.play(i)
+    end
   end
 
-  it "should connect three if possible"
+  it "should connect three if possible" do
+    @player.make_move(@grid)
+    @grid.gamestate.should == :x_wins
+  end
 
-  it "should prevent the opponent from connecting three"
+  it "should prevent the opponent from connecting three" do
+    @grid.play(8)
+    @grid.play(6)
+    @player.make_move(@grid)
+    @grid.history.last.should == 7
+  end
 
 end
