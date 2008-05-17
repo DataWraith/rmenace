8a8de765cf73a3169167c93c186c0e759bae5fa0
Johannes Holzfuß <Drangon@gmx.de>
2008-05-17 17:08:44 +0200

    Fixed MinimaxPlayer by (more or less) completely rewriting it
    It's probably slower now, but it appears to be working.

--- lib/player_minimax.rb
+++ lib/player_minimax.rb
@@ -10,49 +10,53 @@ module TicTacToe
     end
 
     def select_move(grid)
-      search_grid = grid.clone
 
-      i_am_x = (search_grid.to_move == :x)
+      move_eval = []
 
-      best_move = -1
-      best_value = -10
-
-      for i in get_empty_fields(search_grid) do
-        search_grid.play(i)
-
-        value = minimax(search_grid)
-        value *= -1 unless i_am_x
-
-        if (value > best_value) or ((value == best_value) and (rand >= 0.5))
-          best_move = i
-          best_value = value
-        end
-
-        search_grid.undo
-      end
-
-      return best_move
-    end
-
-    def minimax(grid)
-
-      case grid.gamestate
-      when :tie
-        return 0
-      when :x_wins
-        return 1
-      when :o_wins
-        return -1
-      end
-
-      alpha = -1
-      for j in get_empty_fields(grid) do
-        grid.play(j)
-        alpha = [alpha, -minimax(grid)].max
+      for i in get_empty_fields(grid) do
+        grid.play(i)
+        move_eval += [[minimax(grid), i]] # [move value, move number]
         grid.undo
       end
 
-      return alpha
+      if grid.to_move == :x
+        return move_eval.max.last
+      else
+        return move_eval.min.last
+      end
+
+    end
+
+    def evaluate(grid)
+      case grid.gamestate
+      when :tie
+        return 0
+
+      when :x_wins
+        return 10 - grid.move_nr        # Prefer fast victory over slow victory
+
+      when :o_wins
+        return -10 + grid.move_nr       # Prefer slow loss over fast loss
+      end
+    end
+
+    def minimax(grid)
+      if grid.gamestate != :ongoing
+        return evaluate(grid)
+      end
+
+      move_eval = []
+      for i in get_empty_fields(grid)
+        grid.play(i)
+        move_eval += [[minimax(grid), i]]
+        grid.undo
+      end
+
+      if grid.to_move == :x
+        return move_eval.max.first
+      else
+        return move_eval.min.first
+      end
     end
 
   end
