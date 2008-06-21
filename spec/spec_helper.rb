ce0dc833454c396173a46e7c106241156db9a3fb
Johannes Holzfuß <Drangon@gmx.de>
2008-06-21 13:22:25 +0200

    specs: Set the seed for the PRNG in order to get repeatable tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,9 @@ $:.unshift File.join(File.dirname(__FILE__), '..', 'bin')
 $:.unshift File.join(File.dirname(__FILE__), 'grid')
 $:.unshift File.join(File.dirname(__FILE__), 'players')
 
+# Set the Random Number generator, so we get repeatable tests
+srand(0x31415926535)
+
 # helper-function for playing a game
 def play_a_game(player1, player2)
   grid = TicTacToe::Grid.new
